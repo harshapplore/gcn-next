9f88c70165708897899740efd417905a6828fc0a
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-07-23 11:26:28 +0530

    product fix 0.0

--- components/Product/Slider.jsx
+++ components/Product/Slider.jsx
@@ -7,7 +7,7 @@ const Slider = ({ images }) => {
     <div className="slider-container">
       <div className="main-image">
         {images && images.length > 0 && (
-          <img src={images[activeIndex].url} alt={images[activeIndex].name} />
+          <img src={images[activeIndex].url || ""} alt={images[activeIndex].name} />
         )}
       </div>
 
@@ -15,7 +15,7 @@ const Slider = ({ images }) => {
         {images &&
           images.length > 0 &&
           images.map((image, index) => (
-            <img key={image.name} width={150} src={image.formats.thumbnail.url} alt={image.name} onClick={() => setActiveIndex(index)}/>
+            <img key={image.name} width={150} src={image.url || ""} alt={image.name} onClick={() => setActiveIndex(index)}/>
           ))}
       </div>
     </div>
