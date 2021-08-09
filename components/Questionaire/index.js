d1731eb9a0ba4c04762ca1fca32d44391648eb26
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-08-09 15:17:38 +0530

    save data checkpoint

---
+++ components/Questionaire/index.js
@@ -0,0 +1,9 @@
+import Header from "@/shared/Header2";
+import Nav from "@/shared/Nav2";
+import Footer from "@/shared/Footer";
+
+const Questionaire = () => {
+  return <div />;
+};
+
+export default Questionaire;
