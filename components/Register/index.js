08ffc78d7964d1b3a63e57f615af1010333fd807
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-08-12 18:04:40 +0530

    fix axios

--- components/Register/index.js
+++ components/Register/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import axios from "axios";
+import { axios } from "@/setups/axios";
 
 const Register = () => {
   const router = useRouter();
