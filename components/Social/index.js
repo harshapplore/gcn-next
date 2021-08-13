f20ad3f35e24e4f05c09c2399ef5f7d298f6e8a8
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-08-13 11:51:04 +0530

    update social flow

--- components/Social/index.js
+++ components/Social/index.js
@@ -1,16 +1,32 @@
 import { useState, useEffect } from "react";
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { axios } from "@/setups/axios";
 import QS from "query-string";
 
+import Header from "@/shared/Header2";
+import Nav from "@/shared/Nav2";
+import Footer from "@/shared/Footer";
+
+import styles from "./social.module.scss";
+
 const Social = () => {
   const router = useRouter();
+
+  const [provider, setProvider] = useState("");
 
   useEffect(async () => {
     const { provider, ...params } = router.query;
 
+    if (!provider) return;
+
+    setProvider(provider);
+
+    const url = `/auth/${provider}/callback?${QS.stringify(params)}`;
+    console.log(url);
+
     const res = await axios()({
-      url: `/connect/${provider}/callback?${QS.stringify(...params)}`,
+      url,
       method: "GET",
     });
 
@@ -19,6 +35,8 @@ const Social = () => {
     if (res.data) {
       localStorage.setItem("token", res.data.jwt);
 
+      console.log(res.data.jwt);
+
       // Get user details
     }
   }, [router.query]);
@@ -26,9 +44,19 @@ const Social = () => {
   console.log("RQ", router.query);
 
   return (
-    <div className="register-login-message">
-      Authenticating... Please wait!!
-    </div>
+    <>
+      <Head> {provider} Login | Green Cloud Nine </Head>
+      <Header nav={<Nav />} />
+      <div className={styles["provider-container"]}>
+        Authenticating... Please wait!!
+        {/* <div className={styles["input-blocks-ctr"]}>
+          <div className={styles.block}>Customer</div>
+
+          <div className={styles.block}>Seller</div>
+        </div> */}
+      </div>
+      <Footer />
+    </>
   );
 };
 
