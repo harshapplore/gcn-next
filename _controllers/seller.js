f57cc7092c43f874d876b8b983778d8cbf717bec
Alwin Crasta <alwincrasta@Ally.local>
2021-09-23 21:08:36 +0530

    SELLER ORDER PROCESS

--- _controllers/seller.js
+++ _controllers/seller.js
@@ -12,3 +12,13 @@ export const putSeller = async (id, data) => {
 
   return res.data;
 };
+
+
+export const getAllOrders = async (id) => {
+    const res = await authAxios()({
+        url: `/orders?thisShop_contains=${id}&_sort=createdAt:DESC`,
+        method: "GET",
+    });
+
+    return res.data;
+};
