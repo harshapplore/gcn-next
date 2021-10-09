4e60a0464ef8135d6aebf23ad57528f3591561bd
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-10-09 15:43:20 +0530

    loader & placeholder for customer orders list

--- components/CustomerBackend/OrdersList.jsx
+++ components/CustomerBackend/OrdersList.jsx
@@ -5,86 +5,83 @@ import { useRouter } from "next/router";
 import { getAllOrders } from "@/_controllers/customer";
 
 import { ORDER_LIST } from "./routes";
-import Order from './Order.jsx'
+import Order from "./Order.jsx";
 import AppLoader from "@/utils/AppLoader/AppLoader";
 
 const List = ({ order }) => {
-    const [_date, _setDate] = useState();
+  const [_date, _setDate] = useState();
 
-    const router = useRouter();
+  const router = useRouter();
 
-    useEffect(() => {
-        _setDate(new Date(order.createdAt));
-    }, [order.createdAt]);
+  useEffect(() => {
+    _setDate(new Date(order.createdAt));
+  }, [order.createdAt]);
 
-    return (
-        <div className="order-item">
-            <div className="order-row-item">
-                <div>
-                    {_date &&
-                        _date.getDate() +
-                        "." +
-                        (_date.getMonth() < 10 ? "0" : "") +
-                        _date.getMonth() +
-                        "." +
-                        _date.getFullYear()}
-                </div>
-            </div>
-            <div className="order-row-item">
-                <div>{order.id}</div>
-            </div>
-            <div className="order-row-item">
-                <div>{order.status}</div>
-            </div>
-            <div className="order-row-item">
-                <div>€ {order.total}</div>
-            </div>
-            <div
-                className="button-row order-row-item"
-                onClick={() => router.push(ORDER_LIST + `/${order.id}`)}
-            >
-                <a className="button w-button">Details</a>
-            </div>
+  return (
+    <div className="order-item">
+      <div className="order-row-item">
+        <div>
+          {_date &&
+            _date.getDate() +
+              "." +
+              (_date.getMonth() < 10 ? "0" : "") +
+              _date.getMonth() +
+              "." +
+              _date.getFullYear()}
         </div>
-    );
+      </div>
+      <div className="order-row-item">
+        <div>{order.id}</div>
+      </div>
+      <div className="order-row-item">
+        <div>{order.status}</div>
+      </div>
+      <div className="order-row-item">
+        <div>€ {order.total}</div>
+      </div>
+      <div
+        className="button-row order-row-item"
+        onClick={() => router.push(ORDER_LIST + `/${order.id}`)}
+      >
+        <a className="button w-button">Details</a>
+      </div>
+    </div>
+  );
 };
 
 const OrdersList = () => {
-    const { customer } = useSelector((state) => state.customer);
+  const { customer } = useSelector((state) => state.customer);
 
-    const [_orders, _setOrders] = useState([]);
-    const { user } = useSelector((state) => state.user);
+  const [_orders, _setOrders] = useState([]);
+  const { user } = useSelector((state) => state.user);
 
-    useEffect(async () => {
-       
-        getOrders();
-    }, [customer]);
+  const [loadingOrders, setLoadingOrders] = useState(false);
 
-    const getOrders = async () => {
-        // _setOrders(false);
-        const orders = await getAllOrders({ customerId: user._id });
-        console.log(user)
-        console.log(orders)
-        _setOrders(orders);
-    }
+  useEffect(async () => {
+    getOrders();
+  }, [customer]);
 
-    //   const getOrders = () => {
-    //     console.log(user)
-    //   }
+  const getOrders = async () => {
+    setLoadingOrders(true);
+    const orders = await getAllOrders({ customerId: user._id });
+    _setOrders(orders);
+    setLoadingOrders(false);
+  };
 
-    return (
-        <div className="dynamic-content">
-            <div className="heading-wrapper mb-40">
-                <h2>Order History</h2>
-            </div>
-            <div className="order-wrapper">
-                {!_orders && <AppLoader />}
-                {_orders &&
-                    _orders.length > 0 &&
-                    _orders.map((order) => <Order order={order} getOrders={getOrders}/>)}
-            </div>
-        </div>
-    );
+  return (
+    <div className="dynamic-content">
+      <div className="heading-wrapper mb-40">
+        <h2>Order History</h2>
+      </div>
+      <div className="order-wrapper">
+        {loadingOrders && <AppLoader />}
+        {!_orders.length && "You have not placed any orders as of yet."}
+        {_orders &&
+          _orders.length > 0 &&
+          _orders.map((order) => <Order order={order} getOrders={getOrders} />)}
+      </div>
+    </div>
+  );
 };
 
 export default OrdersList;
