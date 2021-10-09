90ba9fafed1d3ab93b91bf526fadc101c77e811e
pavittarx <29155477+pavittarx@users.noreply.github.com>
2021-10-09 15:56:27 +0530

    fav fix

--- components/Products/index.js
+++ components/Products/index.js
@@ -1,11 +1,23 @@
+import { useDispatch } from "react-redux";
+
 import Header from "@/shared/Header2";
 import Nav from "@/shared/Nav2";
 import Footer from "@/shared/Footer";
 import Fetcher from "@/shared/Fetcher";
 
+
+import { fetchFavoriteItems } from "@/slices/favorites";
+
 import View from "./View";
+import { useEffect } from "react";
 
 const ProductsView = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchFavoriteItems());
+  }, []);
+
   return (
     <>
       <Header nav={<Nav />} />
