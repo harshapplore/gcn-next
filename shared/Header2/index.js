f940a507d440dd9be66e5f792eddeeda5488e4f5
Alwin Crasta <alwincrasta@Ally.local>
2021-10-10 15:50:00 +0530

    ALLLY BUG FIXES

--- shared/Header2/index.js
+++ shared/Header2/index.js
@@ -6,9 +6,9 @@ import { useSelector } from "react-redux";
 import Button from "@/shared/Button";
 
 import {
-  Menu,
-  MenuItem,
-  MenuButton
+    Menu,
+    MenuItem,
+    MenuButton
 } from '@szhsin/react-menu';
 import '@szhsin/react-menu/dist/index.css';
 import '@szhsin/react-menu/dist/transitions/slide.css';
@@ -28,113 +28,124 @@ import MobileNav from "../Nav2/MobileNav";
 // }
 
 const Header = ({ nav }) => {
-  const router = useRouter();
-  const { user } = useSelector((state) => state.user);
-  const { errors } = useSelector(state => state.errors);
+    const router = useRouter();
+    const { user } = useSelector((state) => state.user);
+    const { errors } = useSelector(state => state.errors);
+    const { cart } = useSelector(state => state.cart);
 
-  const [showForm, setShowForm] = useState(false);
-  const [search, setSearch] = useState("");
-  const [mobile, setMobile] = useState(false);
+    const [showForm, setShowForm] = useState(false);
+    const [search, setSearch] = useState("");
+    const [mobile, setMobile] = useState(false);
+    const [cartLength, setCartLength] = useState(0)
 
-  useEffect(() => {
-    const { search } = router.query;
-    if (search) setSearch(search);
-  }, [router.query]);
+    useEffect(() => {
+        const { search } = router.query;
+        if (search) setSearch(search);
+    }, [router.query]);
+ 
+    useEffect(() => {
+        if (cart) { 
+            setCartLength(cart.length)
+        } else {
+            setCartLength(0)
+        }
+    }, [cart])
+    
+    const mobileMenu = (e) => {
+        e.preventDefault()
+        setMobile(!mobile)
+    }
+    return (
+        <div className="navbar">
+            {showForm && <AuthForm close={() => setShowForm(false)} />}
 
-  const mobileMenu = (e) => {
-    e.preventDefault()
-    setMobile(!mobile)
-  }
-  return (
-    <div className="navbar">
-      {showForm && <AuthForm close={() => setShowForm(false)} />}
+            <ErrorPopup messages={errors} />
 
-      <ErrorPopup messages={errors} />
+            <div className="navbar-top">
+                <div className="brand-wrapper" onClick={() => router.push("/")}>
+                    <a className="brand w-inline-block w--current cursor">
+                        <img src="/images/fill-2.svg" alt="Green Cloud Nine Logo" />
+                    </a>
+                </div>
+                <div className="form-search w-form">
+                    <div
+                        id="search-form"
+                        name="search-form"
+                        data-name="Search Form"
+                        className="search-bar-wrapper"
+                    >
+                        <input
+                            type="text"
+                            className="search-bar w-input"
+                            placeholder="I'm searching for"
+                            value={search}
+                            onChange={e => setSearch(e.target.value)}
+                            onKeyUp={(e) => e.keyCode === 13 ? router.push(`/products?filters=${JSON.stringify({ search })}`) : ""}
+                        />
+                        <img
+                            src="/images/search-black-24-dp.svg"
+                            alt="search"
+                            className="search-icon cursor"
+                            onClick={() => router.push(`/products?filters=${JSON.stringify({ search })}`)}
+                        />
+                    </div>
+                </div>
+                <div className="clearfix" />
+                <div className="right-icon-nav">
+                    {!user.id && (
+                        <Button
+                            type="secondary"
+                            name="Login / Register"
+                            action={() => setShowForm(true)}
+                        />
+                    )}
+                    <>
+                        {user.id && (
+                            <>
+                                {user.type == "customer" &&
+                                    <a
+                                        className="icon-nav w-inline-block"
+                                        onClick={() => router.push("/customer/favorites")}
+                                    >
 
-      <div className="navbar-top">
-        <div className="brand-wrapper" onClick={() => router.push("/")}>
-          <a className="brand w-inline-block w--current cursor">
-            <img src="/images/fill-2.svg" alt="Green Cloud Nine Logo" />
-          </a>
+                                        <img
+                                            src="/images/favorite-border-black-24-dp.svg"
+                                            loading="lazy"
+                                            alt="icon"
+                                        />
+                                    </a>}
+                                <UserPopup />
+                            </>
+                        )}
+
+                        {user.type !== "seller" && (
+                            <a
+                                className="icon-nav w-inline-block badge-icon"
+                                onClick={() => router.push("/cart")}
+                            >
+                                <span>{cartLength}</span>
+                                <img
+                                    src="/images/local-mall-black-24-dp.svg"
+                                    loading="lazy"
+                                    alt="Shopping Cart"
+                                />
+                            </a>
+                        )}
+                    </>
+                </div>
+                <a onClick={mobileMenu} className="menu-button w-inline-block">
+                    <div className="burger">
+                        <div className="line top" />
+                        <div className="line mid" />
+                        <div className="line bot" />
+                    </div>
+                </a>
+            </div>
+            {/* <NavBarMobile/> */}
+            {/* {nav} */}
+            {mobile ? <MobileNav /> : <>{nav}</>}
         </div>
-        <div className="form-search w-form">
-          <div
-            id="search-form"
-            name="search-form"
-            data-name="Search Form"
-            className="search-bar-wrapper"
-          >
-            <input
-              type="text"
-              className="search-bar w-input"
-              placeholder="I'm searching for"
-              value={search}
-              onChange={e => setSearch(e.target.value)}
-              onKeyUp={(e) => e.keyCode === 13 ? router.push(`/products?filters=${JSON.stringify({ search })}`) : ""}
-            />
-            <img
-              src="/images/search-black-24-dp.svg"
-              alt="search"
-              className="search-icon cursor"
-              onClick={() => router.push(`/products?filters=${JSON.stringify({ search })}`)}
-            />
-          </div>
-        </div>
-        <div className="clearfix" />
-        <div className="right-icon-nav">
-          {!user.id && (
-            <Button
-              type="secondary"
-              name="Login / Register"
-              action={() => setShowForm(true)}
-            />
-          )}
-          <>
-            {user.id && (
-              <>
-                {user.type == "customer" &&
-                  <a
-                    className="icon-nav w-inline-block"
-                    onClick={() => router.push("/customer/favorites")}
-                  >
-
-                    <img
-                      src="/images/favorite-border-black-24-dp.svg"
-                      loading="lazy"
-                      alt="icon"
-                    />
-                  </a>}
-                <UserPopup />
-              </>
-            )}
-
-            {user.type !== "seller" && (
-              <a
-                className="icon-nav w-inline-block"
-                onClick={() => router.push("/cart")}
-              >
-                <img
-                  src="/images/local-mall-black-24-dp.svg"
-                  loading="lazy"
-                  alt="Shopping Cart"
-                />
-              </a>
-            )}
-          </>
-        </div>
-        <a onClick={mobileMenu} className="menu-button w-inline-block">
-          <div className="burger">
-            <div className="line top" />
-            <div className="line mid" />
-            <div className="line bot" />
-          </div>
-        </a>
-      </div>
-      {/* <NavBarMobile/> */}
-      {/* {nav} */}
-      {mobile ? <MobileNav /> : <>{nav}</>}
-    </div>
-  );
+    );
 };
 
 export default Header;
